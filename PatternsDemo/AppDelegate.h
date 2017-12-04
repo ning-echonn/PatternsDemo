fd0a9ef638d5f5d1c9203a6730ce008259f27378
echonn <echonn@centalinedeiMac.local>
2017-12-04 09:38:12 +0800

    Initial Commit

---
+++ PatternsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PatternsDemo
+//
+//  Created by echonn on 2017/12/4.
+//  Copyright © 2017年 echonn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
